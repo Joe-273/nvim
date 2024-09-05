1afdc0d4a298c7609c9abca643a5ded60a552605
QihaoWei <QihaoWei.273@Outlook.com>
2024-09-05 15:17:21 +0800

    add multiCursors plugin

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "rhysd/accelerated-jk", -- fast jk move
+    config = function()
+      vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)")
+      vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)")
+    end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    "ethanholz/nvim-lastplace", -- recur last place
+    config = true,
+  },
+  {
+    "smoka7/multicursors.nvim", -- multi cursors
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
+}
