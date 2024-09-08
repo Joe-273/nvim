a086df283cf9173f79cf1857511c0aeacc44ac56
QihaoWei <QihaoWei.273@Outlook.com>
2024-09-08 20:33:01 +0800

    Clear repository

--- lua/plugins/catppuccin.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  opts = {
-    -- configuration options...
-    transparent_background = true,
-  },
-}
--- lua/plugins/toggleterm.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  opts = function(_, opts) opts.shell = '"C:\\Program Files\\PowerShell\\7\\pwsh.exe"' end,
-}
