efd2c8f93c0cdd9c0e344e3d723bb8c25ff92246
Qihao.PC <QihaoWei.273@gmail.com>
2024-11-16 04:31:37 +0800

    clean

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require("config.keymaps")
 require("config.autocmds")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -22,26 +21,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	-- [[ Configure and install plugins ]]
-	--
-	--  To check the current status of your plugins, run
-	--    :Lazy
-	--
-	--  You can press `?` in this menu for help. Use `:q` to close the window
-	--
-	--  To update plugins you can run
-	--    :Lazy update
-	--
-	-- NOTE: Here is where you install your plugins.
-	--
-	-- Detect tabstop and shiftwidth automatically
 	spec = {
 		{ "tpope/vim-sleuth", event = { "BufReadPost", "BufNewFile" } },
-		-- quick remedy when tab-complete-then-enter fails you, e.g. `nvim init.l`
-
-		-- Use `opts = {}` to force a plugin to be loaded.
 		{ "stevearc/dressing.nvim", event = "VeryLazy", opts = {} },
-
-		-- current best multicursor IMHO
 		{ "mg979/vim-visual-multi", lazy = true, keys = { { "<C-n>", mode = { "n", "x" } } } },
 
 		-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
